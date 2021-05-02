634a14050bfaf738de826d3c2864a6933adda022
David Stockdale <dstock3@gmail.com>
2021-05-02 13:06:54 -0500

    Added JS file.

---
+++ rock.js
@@ -0,0 +1,58 @@
+console.log("Rock, Paper, Scissors...shoot!");
+
+let userInput = prompt("What do you choose?");
+if (userInput != "Rock" || "Paper" || "Scissors") {
+    userInput = prompt("Be serious now! That input is not valid. What do you choose?")
+};
+
+let compInput = (Math.random());
+
+if (compInput  <= .33) {
+    compInput = "Rock";
+    console.log("The computer chooses Rock!")
+};
+
+if (compInput > .33 && compInput <= .66) {
+    compInput = "Paper";
+    console.log("The computer chooses Paper!")
+};
+
+if (compInput > .66) {
+    compInput = "Scissors";
+    console.log("The computer chooses Scissors!")
+};
+
+if (userInput === compInput) {
+    console.log("It's a draw!");
+};
+
+if (userInput === "Rock" && compInput === "Paper") {
+    console.log('You have chosen Rock!')
+    console.log("You lose!")
+};
+
+if (userInput === "Paper" && compInput === "Rock") {
+    console.log("You have chosen Paper!")
+    console.log("You win!")
+};
+
+if (userInput === "Rock" && compInput === "Scissors") {
+    console.log("You have chosen Rock!")
+    console.log("You win!")
+};
+
+if (userInput === "Paper" && compInput === "Scissors") {
+    console.log("You have chosen Paper!")
+    console.log("You lose!")
+};
+
+if (userInput === "Scissors" && compInput === "Rock") {
+    console.log("You have chosen Scissors!")
+    console.log("You lose!")
+};
+
+if (userInput === "Scissors" && compInput === "Paper") {
+    console.log("You have chosen Scissors!")
+    console.log("You win!")
+};
+
