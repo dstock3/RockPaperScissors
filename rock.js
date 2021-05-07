42d74aa193d7329ead1da9b8e77140d22228be1c
David Stockdale <dstock3@gmail.com>
2021-05-06 22:21:17 -0500

    Revised playRound function to return the results of the round instead of logging them in the console.

--- rock.js
+++ rock.js
@@ -23,48 +23,48 @@ function computerPlay() {
 let playerSelection = prompt("What do you choose?");
 playerSelection = playerSelection.toLowerCase();
 
-/* if (userInput == "Rock" || "rock" || "Paper" || "paper" || "Scissors" || "scissors") {
+/* if (userInput !== "Rock" || "rock" || "Paper" || "paper" || "Scissors" || "scissors") {
     userInput = prompt("Be serious now! That input is not valid. What do you choose?")
 } */
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
         console.log('You have chosen rock!')
-        console.log("You lose! Paper covers rock.")
+        return("You lose! Paper covers rock.");
     };
     
     if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("You have chosen paper!")
-        console.log("You win! Paper covers rock.")
+        return("You win! Paper covers rock.");
     };
     
     if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("You have chosen rock!")
-        console.log("You win! Rock beats scissors.")
+        return("You win! Rock beats scissors.");
     };
     
     if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("You have chosen paper!")
-        console.log("You lose! Scissors cuts paper.")
+        return("You lose! Scissors cuts paper.");
     };
     
     if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("You have chosen scissors!")
-        console.log("You lose! Rock beats scissors.")
+        return("You lose! Rock beats scissors.");
     };
     
     if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("You have chosen scissors!")
-        console.log("You win! Scissors cuts paper.")
+        return("You win! Scissors cuts paper.");
     };
     
     if (playerSelection === computerSelection) {
         console.log(`You have chosen ${playerSelection}!`)
-        console.log("It's a draw!");
+        return("It's a draw!");
     };
 }
 
-playRound(playerSelection, computerPlay());
+console.log(playRound(playerSelection, computerPlay()));
 
 
 
