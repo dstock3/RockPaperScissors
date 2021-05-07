80355c01385312ab130eeb4b37ce712b8c95aa60
David Stockdale <dstock3@gmail.com>
2021-05-06 21:40:17 -0500

    Accounting for case sensitivity by converting userInput to lowercase.

--- rock.js
+++ rock.js
@@ -1,6 +1,7 @@
 console.log("Rock, Paper, Scissors...shoot!");
 
 let userInput = prompt("What do you choose?");
+userInput = userInput.toLowerCase();
 
 /* if (userInput == "Rock" || "rock" || "Paper" || "paper" || "Scissors" || "scissors") {
     userInput = prompt("Be serious now! That input is not valid. What do you choose?")
@@ -9,47 +10,47 @@ let userInput = prompt("What do you choose?");
 let compInput = (Math.random());
 
 if (compInput  <= .33) {
-    compInput = "Rock";
-    console.log("The computer chooses Rock!")
+    compInput = "rock";
+    console.log("The computer chooses rock!")
 };
 
 if (compInput > .33 && compInput <= .66) {
-    compInput = "Paper";
-    console.log("The computer chooses Paper!")
+    compInput = "paper";
+    console.log("The computer chooses paper!")
 };
 
 if (compInput > .66) {
-    compInput = "Scissors";
-    console.log("The computer chooses Scissors!")
+    compInput = "scissors";
+    console.log("The computer chooses scissors!")
 };
 
-if (userInput === "Rock" && compInput === "Paper") {
-    console.log('You have chosen Rock!')
+if (userInput === "rock" && compInput === "paper") {
+    console.log('You have chosen rock!')
     console.log("You lose!")
 };
 
-if (userInput === "Paper" && compInput === "Rock") {
-    console.log("You have chosen Paper!")
+if (userInput === "paper" && compInput === "rock") {
+    console.log("You have chosen paper!")
     console.log("You win!")
 };
 
-if (userInput === "Rock" && compInput === "Scissors") {
-    console.log("You have chosen Rock!")
+if (userInput === "rock" && compInput === "scissors") {
+    console.log("You have chosen rock!")
     console.log("You win!")
 };
 
-if (userInput === "Paper" && compInput === "Scissors") {
-    console.log("You have chosen Paper!")
+if (userInput === "paper" && compInput === "scissors") {
+    console.log("You have chosen paper!")
     console.log("You lose!")
 };
 
-if (userInput === "Scissors" && compInput === "Rock") {
-    console.log("You have chosen Scissors!")
+if (userInput === "scissors" && compInput === "rock") {
+    console.log("You have chosen scissors!")
     console.log("You lose!")
 };
 
-if (userInput === "Scissors" && compInput === "Paper") {
-    console.log("You have chosen Scissors!")
+if (userInput === "scissors" && compInput === "paper") {
+    console.log("You have chosen scissors!")
     console.log("You win!")
 };
 
